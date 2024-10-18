d9ae6269b24edc74afe2abdc6670ad342d713d2c
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-10-18 02:42:41 -0700

    Initial commit

---
+++ scripts/reader.py
@@ -0,0 +1,14 @@
+import json
+from jsonreadwriter import JsonReadWriter
+
+# Simple class that skims the characters file and outputs basic data on each character
+class Reader:
+    def __init__(self):
+        pass
+
+def main():
+    pass
+    import config
+
+if __name__ == "__main__":
+    main()
