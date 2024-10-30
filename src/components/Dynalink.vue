1437a9adfedc2e24b7f702718d6de5b72b8142ae
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-10-29 23:04:29 -0700

    Documentation & CSS

--- src/components/Dynalink.vue
+++ src/components/Dynalink.vue
@@ -1,25 +1,33 @@
 <script setup>
+// Dynamic links. Each one constructs a URL dynamically from the props given
 import { ref, computed } from 'vue'
 
 const props = defineProps({
   innerText:{
+    // Text to be rendered inside the a tag
     type: String,
     default: ""
   },
   domain:{
+    // Usually 'https://2e.aonprd.com'
     type: String,
     default: ""
   },
   path:{
+    // Usually something like 'Ancestries.aspx'
     type: String,
     default: ""
   },
   params:{
+    // Constructs the URL's query string.
+    // Key-value pairs to use as URL parameters the given page. For Ancestries.aspx, ID 20 redirects to ratfolk.
+    // The resulting URL of this example would be https://2e.aonprd.com/Ancestries.aspx?ID=20
     type: Object,
     default: {}
   }
 })
 
+// Construct a URL dynamically from the props given
 const url = computed (() => {
   var result = ""
   if (props.innerText !== undefined && props.domain !== undefined &&
@@ -29,12 +37,14 @@ const url = computed (() => {
     var result = props.domain + props.path
     if (props.params){
       result = result.concat("?")
+      // Construct the query string by appending each key-value pair to the URL
       for (const [param, arg] of Object.entries(props.params)){
         result = result.concat(param)
         result = result.concat("=")
         result = result.concat(arg)
         result = result.concat("&")
       }
+      // Slice off the last &
       result = result.slice(0, result.length - 1)
     }
   }
@@ -48,10 +58,23 @@ const url = computed (() => {
 
 <style>
 a {
-  color: #aaccff;
+  /* color: #aaccff; */
+  color: darkblue;
 }
 
 a:visited {
-  color: #ffccff;
+  /* color: #ffccff; */
+  color: darkorchid;
 }
+
+a:hover {
+  background-color: darkblue;
+  color: white;
+}
+
+a:visited:hover {
+  background-color: darkorchid;
+  color: white;
+}
+
 </style>
