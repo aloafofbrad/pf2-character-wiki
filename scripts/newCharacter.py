d5f12d329d5bde8f9837d43f2705ba6462719467
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-11-24 19:19:35 -0800

    Initial commit

---
+++ scripts/newCharacter.py
@@ -0,0 +1,9 @@
+from config import CHARACTER_FILE, DATA_KEY, INFO_KEY
+from Updater import Updater
+from Printer import Printer
+
+def main():
+    updater = Updater(CHARACTER_FILE)
+
+if __name__ == "__main__":
+    main()
