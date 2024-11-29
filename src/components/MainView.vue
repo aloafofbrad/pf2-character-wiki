0d87d39ae67e85d49cefc3479f98b7fcb4c925b9
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-11-28 22:37:39 -0800

    Updated injects, props, models, emits, helper functions; Added DataView

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,275 +1,97 @@
 <script setup>
-import ArtContainer from './components/containers/ArtContainer.vue'
-import Bio from './components/bio/Bio.vue'
-import IndexContainer from './components/containers/IndexContainer.vue'
-import ListContainer from './components/containers/ListContainer.vue'
-import Navbar from './components/Navbar.vue'
-import { ref, reactive, computed } from 'vue'
-import packageJson from "../package.json"
+import ArtContainer from './containers/ArtContainer.vue'
+import DataView from './DataView.vue'
+import IndexContainer from './containers/IndexContainer.vue'
+import ListContainer from './containers/ListContainer.vue'
+import { ref, reactive, computed, inject, watchEffect } from 'vue'
+const DESELECTED = inject('DESELECTED')
+const CATEGORIES = inject('CATEGORIES')
+const props = defineProps({
+  sort_alpha: { type: String, default: "alphabetical" },
+  sort_chrono: { type: String, default: "chronological" },
+  art_view: { type: Number, default: 0, required: true },
+  index_view: { type: Number, default: 1, required: true },
+  list_view: { type: Number, default: 2, required: true },
+  multi_view_enabled: { type: Boolean, default: false },
+  viewMode: { type: Number, required: true },
 
-// import data from './data/characters.json'
-
-// const source = `https://github.com/aloafofbrad/pf2-character-wiki`
-
-// var entries = ref([])
-
-// for (let i = 0; i < data.data.length; i++){
-//   entries.value.push(data.data[i])
-// }
-
-// console.log("entries:")
-// console.log(entries.value)
-
-const DESELECTED = -1
-const SORT_DEFAULT = "default"
-const SORT_ALPHA = "alphabetical"
-const SORT_CHRONO = "chronological"
-const SORT_ALPHA_DESC = "alphabetical_desc"
-const SORT_CHRONO_DESC = "chronological_desc"
-
-const selected = ref(DESELECTED)
-const history = ref([DESELECTED,])
-const index = ref(0)
-const userRegex = ref('')
-const SEARCH_BLEACH = "/[^a-zA-Z0-9 -\?]/g"
-
-const ART_VIEW = 0
-const LIST_VIEW = 1
-const DEFAULT_VIEW = ART_VIEW
-const MULTI_VIEW_ENABLED = true
-const viewMode = ref(DEFAULT_VIEW)
-
-// Comparisons & helpers
-function DNE(arg) {
-  return arg === null || arg === undefined
-}
-
-function compareAgainstDNE(a, b) {
-  // console.log("CompareAgainstDNE: " + a + ", " + b)
-  if (!DNE(a) && DNE(b)) {
-    return -1
-  }
-  if (DNE(a) && !DNE(b)) {
-    return 1
-  }
-  return 0
-}
-
-function compareDataByKey(a, b, key) {
-  if (DNE(a['info'][key]) || DNE(b['info'][key])) {
-    return compareAgainstDNE(a, b)
-  }
-  // console.log(a['info'][key] + " < " + b['info'][key] + "?")
-  if (a['info'][key] < b['info'][key]) {
-    return -1
-  }
-  if (a['info'][key] > b['info'][key]) {
-    return 1
-  }
-  return 0
-}
-
-function compareNames(a, b) {
-  return compareDataByKey(a, b, 'name')
-}
-
-function compareIDs(a, b){
-  return compareDataByKey(a, b, 'id')
-}
-
-// Sorts
-function sortAlphabetically() {
-  sortParadigm.value = SORT_ALPHA
-}
-
-function sortChronologically() {
-  sortParadigm.value = SORT_CHRONO
-}
-
-function deselectEntry() {
-  setSelectedEntry(DESELECTED)
-}
+  /* Add a prop for each category name here. Each one just needs to be a
+  required string. See also the MainView element in the App.vue
+  template. */
+  characterData: { type: String, required: true },
+  journalData: { type: String, required: true },
+  settingData: { type: String, required: true },
+})
+const dataMap = defineModel('dataMap', { type:Object, required: true })
+const selected = defineModel('selected', { type:Number, required: true })
+const category = defineModel('category', { type:String, required: true })
+const emit = defineEmits(['updateSelection'])
 
 function noSelectionMade() {
   return selected.value === DESELECTED
 }
-
-function showArtContainer() {
-  return noSelectionMade() && viewMode.value === ART_VIEW
+function showArt(){
+  return props.viewMode === props.art_view && noSelectionMade()
+}
+function showIndex(){
+  return props.viewMode === props.index_view && noSelectionMade()
+}
+function showList(){
+  return props.viewMode === props.list_view && noSelectionMade()
 }
 
-function showListContainer() {
-  return noSelectionMade() && viewMode.value === LIST_VIEW
-}
-
-function toggleView() {
-  if (MULTI_VIEW_ENABLED){
-    if (viewMode.value === ART_VIEW){
-      viewMode.value = LIST_VIEW
-      sortAlphabetically()
+/* Helper for dataMap
+    Tests if a key exists in dataMap
+    args:
+    key   the key to test
+*/
+function isAValidKey(key) {
+  // const result = CATEGORIES().find((k) => k === key);
+  // return result
+  for (let i = 0; i < CATEGORIES.length; i++){
+    var curr = CATEGORIES[i];
+    // console.log(curr)
+    if (key === curr){
+      return true
     }
-    else if (viewMode.value === LIST_VIEW) {
-      viewMode.value = ART_VIEW
-      sortChronologically()
-    }
   }
-  updateSelection(DESELECTED)
+  return false
 }
 
-function isAValidId(id) {
-  // console.log("validating id\t" + id + "\tagainst\t" + (entries.value.length - 1))
-  return (id >= 0 && id <= (entries.value.length - 1))
-}
-
-function setSelectedEntry(id) {
-  if (isAValidId(id) && id !== selected.value){
-    selected.value = id
-  }
-  else{
-    selected.value = DESELECTED
-  }
-  console.log(`NOW VIEWING ENTRY: ${id}`)
+function isAValidId(id, category) {
+  return id === DESELECTED || (id >= 0 && id < dataMap.value[category].length);
 }
 
 function getSelectedEntry() {
-  if (noSelectionMade()) {
-    return entries.value[0]
-  }
-  // console.log("checking " + 0 + " thru " + entries.length + "...")
-  for (let i = 0; i < entries.value.length; i++){
-    if (selected.value === entries.value[i].id){
-      // console.log("found " + entries[i].info['name'])
-      return entries.value[i]
-    }
-  }
-  return entries.value[0]
-}
-
-// History management
-function isAValidHistoricalIndex(value) {
-  return (value >= 0 && value <= (history.value.length - 1))
-}
-
-function goBack() {
-  setIndex(index.value - 1)
-  setSelectedEntry(history.value[index.value])
-}
-
-function goForward() {
-  setIndex(index.value + 1)
-  setSelectedEntry(history.value[index.value])
-}
-
-function setIndex(value) {
-  // var old = 0 + index.value
-  // console.log(`history index, prev: ${old}`)
-  if (!isAValidHistoricalIndex(value)){
-    return
-  }
-  index.value = value
-  // console.log(`history index, curr: ${index.value}`)
-}
-
-function pushHistory(curr) {
-  history.value.push(curr)
-  setIndex(index.value + 1)
-  // console.log(`history: ${history.value}`)
-}
-
-function cleanHistory() {
-  /* Removes instances of DESELECTED from the history so that
-  when goBack() and goForward() are called, DESELECTED is only
-  shown at the beginning and end of the history. As such,
-  the for loop ignores indices 0 and (n-1)
-  
-  Calling this function in updateSelection() and HandleBioClose() 
-  because those will probably be called much less often than 
-  goBack() and goForward() */
-  var oldLength = history.value.length
-  var newHistory = [DESELECTED,]
-  for (let i = 1;i < oldLength - 1;i++){
-    var curr = history.value[i]
-    if (curr !== DESELECTED){
-      newHistory.push(curr)
-    }
-    else if (i !== 0 && curr === DESELECTED){
-      setIndex(index.value - 1)
-    }
-  }
-  newHistory.push(history.value[oldLength - 1])
-  history.value = newHistory
-}
-
-function updateSelection(id) {
-  if (id === selected.value){
-    pushHistory(DESELECTED)
-  }
-  else{
-    pushHistory(id)
-  }
-  setSelectedEntry(id)
-  cleanHistory()
-  console.log(`history.value: ${history.value}`)
-}
-
-function handleBioClose(){
-  // pushHistory(DESELECTED)
-  deselectEntry()
-  cleanHistory()
-  console.log(`history.value: ${history.value}`)
-}
-
-// Search
-function updateSearch(query) {
-  userRegex.value = cleanString(query)
-}
-
-function cleanString(s){
-  return s.replace(SEARCH_BLEACH, "")
-}
-
-const sortParadigm = ref(SORT_CHRONO)
-const sortParadigms = reactive({
-  [SORT_ALPHA]: compareNames,
-  [SORT_CHRONO]: compareIDs
-})
-
-function sort(list) {
-  var paradigm = sortParadigms[sortParadigm.value]
-  if (paradigm === null || paradigm === undefined){
-    return list.slice()
-  }
-  return list.slice().sort((a, b) => paradigm(a, b))
-}
-
-function matchesRegex(s, regex){
-  try {
-    var result = regex.test(s)
-    return result
-  }
-  catch (e){
-    console.log("Expected error in matchesRegex")
-    console.log(e)
-  }
-  return false
-
-}
-
-function filter(list){
-  if (userRegex.value === "" || userRegex.value === null || userRegex === undefined){
-    return list.slice()
-  }
-  var regex = new RegExp(userRegex.value.toLowerCase())
-  console.log("regex\t" + regex)
-  return list.filter(entry => matchesRegex(entry['info']['name'].toLowerCase(), regex))
+  // var result = dataMap.value[props.category].at(selected.value + 1) // works; saved as comment for posterity/sanity
+  var result = dataMap.value[props.category].at(selected.value + 1) // works
+  return result
 }
 
 const arranged = computed(() => {
-  var sorted = sort(entries.value)
-  var filtered = filter(sorted)
-  console.log(filtered.slice())
-  return filtered.slice()
+  if (isAValidKey(props.category)){
+    console.log("props.category is valid")
+    console.log(dataMap.value[props.category])
+    return dataMap.value[props.category]
+    // return dataMap.value[props.category].filter(entry => matchesRegex(entry['info']['name']).toLowerCase(), regex);
+  }
 })
+
+const debug = computed(() => {
+  var result = `MainView Stats: | viewMode: ${props.viewMode} | selected.value: ${selected.value} | noSelectionMade(): ${noSelectionMade()}`
+  result = result.concat(` | showArt(): ${showArt()} | showIndex(): ${showIndex()} | showList(): ${showList()}`)
+  return result
+})
+
+function updateSelection(id, category) {
+  if (isAValidKey(category)){
+    if (isAValidId(id, category)){
+      console.log("updateSelection()", id, ", ", category)
+      emit('updateSelection', id, category)
+    }
+  }
+  // if id or category isn't valid, there's no need to update!
+}
 </script>
 
 <template>  
@@ -280,23 +102,25 @@ const arranged = computed(() => {
      for each element, even if one element is removed. To make changes to how these containers and their
      respective elements are shown, you need to modify the file(s) of whichever container(s) you plan to
      use. See the top of this file for file locations, in the imports section. -->
-    <ArtContainer v-show="showArtContainer()" :entries="arranged" @update-selection="updateSelection"/>
-    <!-- <ListContainer v-show="showListContainer()" :entries="arranged" @update-selection="updateSelection"/> -->
-    <IndexContainer v-show="showListContainer()" :entries="arranged" @update-selection="updateSelection"/>
-    <!-- end Containers -->
-    <!-- Bio
-     To change data shown in the Bio, or the way that data is show, you'll need to modify the file for
-     the Bio component (again, see the top of this file for its location, in the imports section).
-     Don't change the v-show binding here, regardless of which containers are shown. -->
-    <div id="bio" v-show="!noSelectionMade()">
-      <Bio
-        :ID="selected.value"
-        :info="getSelectedEntry().info"
-        @deselect-entry="handleBioClose()"
-        @select-entry="updateSelection(entry.id)">
-      </Bio>
+    <div>
+      <h3>{{ debug }}</h3>
     </div>
-    <!-- end Bio -->
+    <ArtContainer v-show="showArt()" :entries="arranged" :category="category" @update-selection="updateSelection"></ArtContainer>
+    <IndexContainer v-show="showIndex()" :entries="arranged" @update-selection="updateSelection"></IndexContainer>
+    <ListContainer v-show="showList()" :entries="arranged" @update-selection="updateSelection"></ListContainer>
+    <!-- end Containers -->
+    <!-- DataView
+      This component is in charge of displaying data.
+      For each category of data you have (characters, etc), you need to
+      write a binding for it and pass the  -->
+    <DataView v-show="!noSelectionMade()"
+      v-model:selected="selected" v-model:category="category"
+      :entry="getSelectedEntry()"
+      :characterData="props.characterData"
+      :journalData="props.journalData"
+      :settingData="props.settingData"
+      @update-selection="updateSelection">
+    </DataView>
   </div>
 </template>
 
