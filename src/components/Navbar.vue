1419df5603610509004b29082fd11d89eb3aa0b7
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-11-29 23:49:02 -0800

    Updated emits; added category, isAValidView(), buttons for containers, emits for view switching; removed toggleView(), viewToggleText

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,177 +2,145 @@
 import { ref, computed } from 'vue'
 
 const props = defineProps({
-    history: {
-        // Type: Array,
-        Default: [],
-        Required: false
-    },
-    index: {
-        // Type: Number,
-        Default: -1
-    },
-    version: {
-        Type: String,
-        Default: ""
-    },
-    source: {
-        Type: String,
-        Default: ""
-    },
-    searchQuery: {
-        Type:String,
-        Default: ""
-    },
-    view: {
-        Type: Number
-    },
-    art_view: {
-        Type: Number
-    },
-    list_view: {
-        Type: Number
-    },
-    multi_view_enabled: {
-        Type: Boolean
-    }
+  history: { Type: Array, Default: [], Required: false },
+  index: { Type: Number, Default: -1 },
+  version: { Type: String, Default: "" },
+  source: { Type: String, Default: "" },
+  searchQuery: { Type:String, Default: "" },
+  view: { Type: Number, required: true },
+  art_view: { Type: Number, required: true },
+  list_view: { Type: Number, required: true },
+  index_view: { Type: Number, required: true },
+  validViews: { Type: Array, required: true }
 })
-const emit = defineEmits(['goBack', 'goForward', 'toggleView'])
+const emit = defineEmits(['goBack', 'goForward', 'goToView', 'sortAlphabetically', 'sortChronologically', 'updateSearch'])
+const category = defineModel({ Type: String, required: true })
 
 const searchQuery = ref('')
 const debug = true
 
-const viewToggleText = computed(() => {
-    var artText = "View List"
-    var listText = "View Art"
-    if (props.view === props.art_view){
-        return artText
-    }
-    else if (props.view === props.list_view){
-        return listText
-    }
-    return artText
-})
-
 function historyExists() {
-    var result = props.history !== null && props.history !== undefined
-    if (debug) {
-        if (!result) {
-            console.log("Navbar didn't get history")
-        }
-        console.log(`history (navbar): ${props.history}`)
+  var result = props.history !== null && props.history !== undefined
+  if (debug) {
+    if (!result) {
+      console.log("Navbar didn't get history")
     }
-    return result
+    console.log(`history (navbar): ${props.history}`)
+  }
+  return result
 }
 
 function indexExists() {
-    var result = props.index !== null && props.index !== undefined
-    if (debug) {
-        if (!result) {
-            console.log("Navbar didn't get index")
-        }
-        console.log(`history index (navbar): ${props.index}`)
+  var result = props.index !== null && props.index !== undefined
+  if (debug) {
+    if (!result) {
+      console.log("Navbar didn't get index")
     }
-    return result
+    console.log(`history index (navbar): ${props.index}`)
+  }
+  return result
 }
 
 function canGoBack() {
-    var result = false
-    if (!historyExists() || !indexExists()){
-        return false
-    }
-    result = props.index > 0
-    return result
+  var result = false
+  if (!historyExists() || !indexExists()){
+    return false
+  }
+  result = props.index > 0
+  return result
 }
 
 function canGoForward() {
-    var result = false
-    if (!historyExists() || !indexExists()){
-        return false
-    }
-    result = props.index < (props.history.length - 1)
-    return result
+  var result = false
+  if (!historyExists() || !indexExists()){
+    return false
+  }
+  result = props.index < (props.history.length - 1)
+  return result
 }
 
-function getHTMLElement(id){
-    var element = document.getElementById(id)
-    return element
-}
+function getHTMLElement(id){ return document.getElementById(id) }
 
 function updateElementStyle(element, property, value){
-    if (element !== null && element !== undefined){
-        try{
-            // element.style.setProperty("filter", "grayscale(1.0)")
-            element.style.setProperty(property, value)
-        }
-        catch{}
+  if (element !== null && element !== undefined){
+    try{
+      // element.style.setProperty("filter", "grayscale(1.0)")
+      element.style.setProperty(property, value)
     }
+    catch{}
+  }
 }
 
 function updateHistoryButtons(){
-    var back = getHTMLElement("backButton")
-    var forward = getHTMLElement("forwardButton")
-    let filterValue = "grayscale(100%)"
-    let noFilter = "none"
-    if (canGoBack()){
-        updateElementStyle(back, "filter", noFilter)
-    }
-    else {
-        updateElementStyle(back, "filter", filterValue)
-    }
-    if (canGoForward()){
-        updateElementStyle(forward, "filter", noFilter)
-    }
-    else {
-        updateElementStyle(forward, "filter", filterValue)
-    }
+  var back = getHTMLElement("backButton")
+  var forward = getHTMLElement("forwardButton")
+  let filterValue = "grayscale(100%)"
+  let noFilter = "none"
+  if (canGoBack()){
+    updateElementStyle(back, "filter", noFilter)
+  }
+  else {
+    updateElementStyle(back, "filter", filterValue)
+  }
+  if (canGoForward()){
+    updateElementStyle(forward, "filter", noFilter)
+  }
+  else {
+    updateElementStyle(forward, "filter", filterValue)
+  }
 }
 
-function goBack(){
-    if (canGoBack()){
-        emit('goBack')
-    }
-}
-
-function goForward(){
-    if (canGoForward()){
-        emit('goForward')
-    }
-}
-
+function goBack(){ if (canGoBack()){ emit('goBack') } }
+function goForward(){ if (canGoForward()){ emit('goForward') } }
 function move(direction){
-    if (direction === 'back'){
-        goBack()
-    }
-    if (direction === 'forward'){
-        goForward()
-    }
-    updateHistoryButtons()
+  if (direction === 'back'){ goBack() }
+  if (direction === 'forward'){ goForward() }
+  updateHistoryButtons()
 }
 
-function toggleView() {
-    emit('toggleView')
+function isAValidView(num){
+  for (let i = 0; i < props.validViews.length; i++){
+    if (num === props.validViews[i]){ return true }
+  }
+  return false
 }
+
+const showArtButton = computed(() => {
+  return isAValidView(props.art_view) && props.view !== props.art_view
+})
+const showIndexButton = computed(() => {
+  return isAValidView(props.index_view) && props.view !== props.index_view
+})
+const showListButton = computed(() => {
+  return isAValidView(props.list_view) && props.view !== props.list_view
+})
+function goToView(view) { emit('goToView', view) }
+function goToArt() { goToView(props.art_view) }
+function goToIndex() { goToView(props.index_view) }
+function goToList() { goToView(props.list_view) }
 </script>
 
 <template>
   <div class="Navbar">
-    <!-- Left-aligned elements -->
+  <!-- Left-aligned elements -->
     <form id="NavbarSearch" class="NavButton prevent-select">
-        Search <input name="query" v-model.trim="searchQuery" placeholder="you know you want to... :3" autocomplete="off" @submit.prevent="onSubmit" @keyup="$emit('updateSearch', searchQuery)"/>
+      Search <input name="query" v-model.trim="searchQuery" placeholder="you know you want to... :3" autocomplete="off" @submit.prevent="onSubmit" @keyup="$emit('updateSearch', searchQuery)"/>
     </form>
     <div class="NavButton prevent-select" @click="$emit('sortAlphabetically')">
-        ■Sort 🅰
+      ■Sort 🅰
     </div>
     <div class="NavButton prevent-select" @click="$emit('sortChronologically')">
-        ■Sort ⌛
+      ■Sort ⌛
     </div>
     <div class="NavButton prevent-select historyButton" id="backButton" @click="move('back')">
-        ■Back ⏪
+      ■Back ⏪
     </div>
     <div class="NavButton prevent-select historyButton" id="forwardButton" @click="move('forward')">
-        ■Next ⏩
+      ■Next ⏩
     </div>
-    <div class="NavButton prevent-select" v-if="props.multi_view_enabled" @click="toggleView()">■{{ viewToggleText }}</div>
+    <div class="NavButton prevent-select" v-if="showArtButton" @click="goToArt()">■Art</div>
+    <div class="NavButton prevent-select" v-if="showIndexButton" @click="goToIndex()">■Index</div>
+    <div class="NavButton prevent-select" v-if="showListButton" @click="goToList()">■List</div>
     <!-- Right-aligned elements -->
     <div class="NavButton prevent-select NavButtonRight"><a :href="props.source">■Source Code</a></div>
     <div class="NavButton prevent-select NavButtonRight">Version {{ version }}</div>
@@ -183,112 +151,112 @@ function toggleView() {
 <style>
 
 .Navbar {
-    min-height: 24px;
-    overflow-y: hidden;
-    font-family: monospace;
-    overflow-y: hidden;
-    width: 100%;
-    justify-content: space-between;
-    padding: unset;
-    backdrop-filter: blur(8px);
+  min-height: 24px;
+  overflow-y: hidden;
+  font-family: monospace;
+  overflow-y: hidden;
+  width: 100%;
+  justify-content: space-between;
+  padding: unset;
+  backdrop-filter: blur(8px);
 
-    a {
-        text-decoration: none;
+  a {
+    text-decoration: none;
+  }
+
+  .NavButton {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    float: left;
+    font-family: inherit;
+
+    * {
+      display: inherit;
+      flex-flow: inherit;
+      justify-content: inherit;
+      align-items: inherit;
+      font-family: inherit;
     }
 
-    .NavButton {
-        display: flex;
-        flex-flow: column nowrap;
-        justify-content: center;
-        align-items: center;
-        float: left;
-        font-family: inherit;
-
-        * {
-            display: inherit;
-            flex-flow: inherit;
-            justify-content: inherit;
-            align-items: inherit;
-            font-family: inherit;
-        }
-
-        &:hover {
-            background-color: white;
-            opacity: 1.0;
-            color: black;
-        }
-
-        input {
-            font-size: smaller;
-            font-family: monospace;
-            border: 1px solid white;
-            margin-left: 0.5em;
-            height: 12px;
-            width: 16em;
-            outline: none;
-
-            &:hover {
-                background-color: white;
-                opacity: 1.0;
-                color: black;
-                border: 1px solid black;
-            }
-
-            &[type=text]:focus {
-                border: 1px solid black;
-            }
-
-            &::placeholder {
-                color: #fff;
-                opacity: 1;
-            }
-        }
+    &:hover {
+      background-color: white;
+      opacity: 1.0;
+      color: black;
     }
 
-    .NavButton, form {
-        padding-left: 8px;
-        padding-right: 8px;
-    }
+    input {
+      font-size: smaller;
+      font-family: monospace;
+      border: 1px solid white;
+      margin-left: 0.5em;
+      height: 12px;
+      width: 16em;
+      outline: none;
 
-    .NavButton, form, input, a {
-        height: 24px;
-    }
+      &:hover {
+        background-color: white;
+        opacity: 1.0;
+        color: black;
+        border: 1px solid black;
+      }
 
-    .NavButtonRight {
-        float: right;
-    }
+      &[type=text]:focus {
+        border: 1px solid black;
+      }
 
-    &:last-child {
-        margin-left:auto;
+      &::placeholder {
+        color: #fff;
+        opacity: 1;
+      }
     }
+  }
+
+  .NavButton, form {
+    padding-left: 8px;
+    padding-right: 8px;
+  }
+
+  .NavButton, form, input, a {
+    height: 24px;
+  }
+
+  .NavButtonRight {
+    float: right;
+  }
+
+  &:last-child {
+    margin-left:auto;
+  }
 }
 
 .Navbar {
-    color: white;
-    background-color: black;
-    opacity: 0.75;
+  color: white;
+  background-color: black;
+  opacity: 0.75;
 
-    .NavButton {
-        color: inherit;
-        background-color: inherit;
-        opacity: inherit;
+  .NavButton {
+    color: inherit;
+    background-color: inherit;
+    opacity: inherit;
 
-        * {
-            color: inherit;
-            background-color: inherit;
-            opacity: inherit;
-        }
+    * {
+      color: inherit;
+      background-color: inherit;
+      opacity: inherit;
     }
+  }
 }
 
 #NavbarSearch {
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
 }
 
 .historyButton {
-    filter: grayscale(100%);
+  filter: grayscale(100%);
 }
 </style>
