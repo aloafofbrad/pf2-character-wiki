22a60b969ae4248580712c79ff192787ba938d41
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-12-01 00:53:57 -0800

    Added maxID, displayKey, display()

--- src/components/containers/ListContainer.vue
+++ src/components/containers/ListContainer.vue
@@ -3,13 +3,16 @@ import Tag from '../Tag.vue';
 import { ref, computed } from 'vue'
 const props = defineProps({
   entries: { Type: Array, default: [] },
-  category: { Type: String, required: true }
+  category: { Type: String, required: true },
+  maxID: { type: Number, required: true },
+  displayKey: { type: String, required: true },
 })
-
 const emit = defineEmits(['updateSelection'])
 
 function isAValidId(id) {
-  return (id >= 0 && id <= (props.entries.length - 1))
+  // return (id >= 0 && id <= (props.entries.length - 1))
+  console.log(`id (${id}) < props.maxID (${props.maxID})`)
+  return (id >= 0 && id < props.maxID)
 }
 
 function entryClick(id) {
@@ -17,12 +20,14 @@ function entryClick(id) {
   emit('updateSelection', id, props.category)
 }
 
+function display(entry) { return entry.info[props.displayKey] }
+
 </script>
 
 <template>
   <div class="listContainer">
     <Tag v-show="isAValidId(entry.id)" v-for="entry in props.entries" @click="entryClick(entry.id)">
-      {{ entry.info.name }}
+      {{ display(entry) }}
     </Tag>
   </div>
 </template>
