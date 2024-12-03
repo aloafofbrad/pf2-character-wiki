1f8f01038f8a9be3a9450acb53bd13ec2966909e
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-12-02 20:31:26 -0800

    Updated with note to self

--- scripts/rearrangeBiographies.py
+++ scripts/rearrangeBiographies.py
@@ -1,9 +1,34 @@
+"""
+Script to replace the "biographies" object (a list of strings) with a 
+list of objects called "contents". End result should look like:
+{
+  "id":-1,
+  "info":{
+    ...
+    "contents":[
+      "author":{
+        "name":"",
+        "id":null,
+        "date":"",
+        "addendum":""
+      },
+      "text":[
+        "some strings that",
+        "people want to read"
+      ]
+    ],
+    ...
+  }
+}
+
+Ran successfully on 12/2/2024
+"""
+
 from config import CHARACTER_FILE, DATA_KEY, INFO_KEY
 from Updater import Updater
 from Printer import Printer
 
 def main():
-    # newBioTemplate = dict()
     newBioTemplate = [{
         "author":{
             "name":"",
