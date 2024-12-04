b7c8d55f8e6f1c7f0cb23fcc8b41cda7741a6a5a
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-12-04 06:19:35 -0800

    Updated rendering logic, made component actually functional

--- src/components/entries/Status.vue
+++ src/components/entries/Status.vue
@@ -1,66 +1,66 @@
 <script setup>
+import Tag from '../Tag.vue';
 import { ref, computed } from 'vue'
 
+/* Props
+    value             The value to be rendered
+    type              The type associated with the instance of this
+                      component
+    showableValues    If props.value is in this, the component will be 
+                      rendered. Overrides hideableValues.
+    hideableValues    If props.value is in this, the component won't be
+                      rendered. Overridden by showableValues.
+    checkType         If true, showable() checks that props.type is in
+                      showableValues and not in hideableValues.
+    renderTag         If true, renders this component within a Tag.
+ */
 const props = defineProps({
-  value: {
-    type:String,
-    default:""
-  },
-  type: {
-    type:String,
-    default:""
-  },
-  parentDisplay: {
-    type:Boolean,
-    default:true
-  },
-  defaultValue: {
-    type:String,
-    default:""
-  }
+  value: { type:String, default: "" },
+  type: { type:String, default: "Normal", required: false },
+  showableValues: { type:Array, required: false, default: ["Player", 
+  "Normal", "Dead", "In Party"
+  ] },
+  hideableValues: { type:Array, required: false, default: ["Generic"] },
+  checkType: { type:Boolean, default: false, required: false },
+  renderTag: { type:Boolean, default: false, required: false },
 })
 
-const parentElementId = "status"
-
-function updateParentDisplay(show) {
-  var parent = document.getElementById(parentElementId)
-  if (show){
-    parent.style.setProperty("display", "unset")
+function isIn(value, target){
+  for (let i = 0; i < target.length; i++){
+    if (value === target.at(i)){ return true }
   }
-  else{
-    parent.style.setProperty("display", "none")
-  }
+  return false
 }
 
-function showable() {
-  var result = false
-  if ((props.type === "Player" || props.type === "Normal") || (props.type !== "Generic" && props.type !== "Group") && props.value !== "?"){
-    result = true
-  }
-  if (props.parentDisplay) {
-    updateParentDisplay(result)
+function isInShowables(value){ return isIn(value, props.showableValues) }
+function isInHideables(value){ return isIn(value, props.hideableValues) }
+
+function showable(){
+  var result = true
+  if (isInHideables(props.value)){ result = false }
+  if (isInShowables(props.value)){ result = true }
+  if (props.checkType){
+    if (isInHideables(props.type)){ result = false }
+    if (isInShowables(props.type)){ result = true }
   }
   return result
 }
 
+function renderTag(){
+  return props.renderTag && showable()
+}
+
 const innerText = computed(() => {
-  var result = "Status: "
+  var result = `Status: `
   result = result.concat(`${props.value}`)
   return result
 })
 </script>
 
 <template>
-  <p v-show="showable()">{{ innerText }}</p>
+  <Tag v-if="renderTag()"><p>{{ innerText }}</p></Tag>
+  <p v-else-if="showable()">{{ innerText }}</p>
 </template>
 
 <style>
-#characterName {
-  text-align: center;
-  width: 100%;
-  margin: 0;
-  margin-left: 3em;
-  margin-right: 3em;
-  word-wrap: break-word;
-}
 </style>
