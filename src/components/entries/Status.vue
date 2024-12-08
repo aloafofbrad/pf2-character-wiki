eeaa4302d8b8c24de3a7fdd80c44e2556693c1c0
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-12-07 18:32:24 -0800

    Reorganized component; simpler logic in isIn(); more clear function names

--- src/components/entries/Status.vue
+++ src/components/entries/Status.vue
@@ -3,52 +3,47 @@ import Tag from '../Tag.vue';
 import { ref, computed } from 'vue'
 
 /* Props
-    value             The value to be rendered
-    type              The type associated with the instance of this
-                      component
-    showableValues    If props.value is in this, the component will be 
-                      rendered. Overrides hideableValues.
-    hideableValues    If props.value is in this, the component won't be
-                      rendered. Overridden by showableValues.
-    checkType         If true, showable() checks that props.type is in
-                      showableValues and not in hideableValues.
-    renderTag         If true, renders this component within a Tag.
+    value       The value to be rendered
+    type        The type associated with the instance of this component
+    
+    showables   If props.value is in this, the component will be 
+                rendered. Overrides hideables; if a value is in both 
+                lists, the component will render.
+    hideables   If props.value is in this, the component won't be 
+                rendered. Overridden by showables.
+    
+    checkType   If true, isShowable() checks that props.type is in 
+                showables and not in hideables. Overrides props.value,
+                regardless of which lists props.value is in.
+    renderTag   If true, renders this component within a Tag.
  */
 const props = defineProps({
   value: { type:String, default: "" },
   type: { type:String, default: "Normal", required: false },
-  showableValues: { type:Array, required: false, default: ["Player", 
-  "Normal", "Dead", "In Party"
-  ] },
-  hideableValues: { type:Array, required: false, default: ["Generic"] },
+  showables: { type:Array, default: [], required: false },
+  hideables: { type:Array, default: [], required: false },
   checkType: { type:Boolean, default: false, required: false },
   renderTag: { type:Boolean, default: false, required: false },
 })
 
-function isIn(value, target){
-  for (let i = 0; i < target.length; i++){
-    if (value === target.at(i)){ return true }
-  }
-  return false
-}
+function isInList(value, target){ return target.includes(value) }
 
-function isInShowables(value){ return isIn(value, props.showableValues) }
-function isInHideables(value){ return isIn(value, props.hideableValues) }
-
-function showable(){
+function isShowable(){
   var result = true
-  if (isInHideables(props.value)){ result = false }
-  if (isInShowables(props.value)){ result = true }
+
   if (props.checkType){
-    if (isInHideables(props.type)){ result = false }
-    if (isInShowables(props.type)){ result = true }
+    if (isInList(props.type, props.hideables)){ result = false }
+    if (isInList(props.type, props.showables)){ result = true }
   }
+  else {
+    if (isInList(props.value, props.hideables)){ result = false }
+    if (isInList(props.value, props.showables)){ result = true }
+  }
+
   return result
 }
 
-function renderTag(){
-  return props.renderTag && showable()
-}
+function renderTag(){ return props.renderTag && isShowable() }
 
 const innerText = computed(() => {
   var result = `Status: `
@@ -59,7 +54,7 @@ const innerText = computed(() => {
 
 <template>
   <Tag v-if="renderTag()"><p>{{ innerText }}</p></Tag>
-  <p v-else-if="showable()">{{ innerText }}</p>
+  <p v-else-if="isShowable()">{{ innerText }}</p>
 </template>
 
 <style>
