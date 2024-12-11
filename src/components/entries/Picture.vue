d65b12dc79eb474528dfcef4a8761da8d409596f
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-12-11 05:58:54 -0800

    Added conditional rendering in case entries have an empty image field

--- src/components/entries/Picture.vue
+++ src/components/entries/Picture.vue
@@ -1,28 +1,40 @@
 <script setup>
-import { ref, computed } from 'vue'
-
+import { computed, inject } from 'vue'
+const exists = inject('exists')
 const props = defineProps({
-  src:{
-    Type: String,
-    Default: "public/silhouette.jpg"
-  }
+  src:{ Type:String, default:"", required:true },
+  placeholder: { Type:String, default:"", required:false },
+  alt:{ Type:String, default:"", required:false },
+  renderOnPlaceholder: { Type:Boolean, default: true, required: false}
 })
-// function imageUrl() { return new URL(`/${props.info.image}`, import.meta.url) }
 
+function isEmptyString(s){ return s === "" }
+
+// Determines if props.src is "renderable"
+const sourceIsRenderable = computed(() => {
+  return exists(props.src) && !isEmptyString(props.src)
+})
+
+// Returns the URL to be bound in the img element
 const source = computed(() => {
-  return new URL(`/${props.src}`, import.meta.url)
+  if (sourceIsRenderable.value){
+    return new URL(`/${props.src}`, import.meta.url)
+  }
+  return new URL(`/${props.placeholder}`, import.meta.url)
+})
+
+/* Checks if the component should be rendered at all. Returns true
+if either src is a valid URL, or if the component is set to render
+when a URL to a placeholder image is given in props.placeholder */
+const isShowable = computed(() => {
+  if (sourceIsRenderable.value){ return true }
+  return props.renderOnPlaceholder
 })
 </script>
 
 <template>
-  <img id="pic" :src="source" alt="img"/>
+  <img v-if="isShowable" :src="source" :alt="props.alt"/>
 </template>
 
 <style>
-#pic {
-  width: 256px;
-  margin-left: 48px;
-  margin-right: 48px;
-  border-radius: 4px;
-}
 </style>
