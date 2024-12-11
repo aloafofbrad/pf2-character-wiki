bf6d390cd78ea1489bb7bf9af926fc9d959a7aef
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-12-11 05:57:30 -0800

    import SETTINGS_FILE

--- scripts/rearrangeBiographies.py
+++ scripts/rearrangeBiographies.py
@@ -24,7 +24,7 @@ list of objects called "contents". End result should look like:
 Ran successfully on 12/2/2024
 """
 
-from config import CHARACTER_FILE, DATA_KEY, INFO_KEY
+from config import CHARACTER_FILE, SETTINGS_FILE, DATA_KEY, INFO_KEY
 from Updater import Updater
 from Printer import Printer
 
@@ -41,11 +41,10 @@ def main():
         ]
     },]
 
-    characterUpdater = Updater(CHARACTER_FILE, key="contents", validation=False, value=newBioTemplate, condition=None, arguments=[], dryRun=False)
-    characterUpdater.create()
-    characterUpdater.transplantField(["biography", 0], ["contents", 0, "text"])
-    characterUpdater.setKey("biography")
-    characterUpdater.delete()
+    settingUpdater = Updater(SETTINGS_FILE, key="contents", validation=False, value=newBioTemplate, condition=None, arguments=[], dryRun=False)
+    settingUpdater.create()
+    settingUpdater.setKey("history")
+    settingUpdater.delete()
 
 if __name__ == "__main__":
     main()
