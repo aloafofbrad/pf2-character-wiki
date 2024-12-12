45d9e0fd1b3359000f6e3a35051e4927fe972926
aloafofbrad <30997096+aloafofbrad@users.noreply.github.com>
2024-12-12 03:30:02 -0800

    Generalized tooltips code

--- src/components/entries/CharacterType.vue
+++ src/components/entries/CharacterType.vue
@@ -1,30 +1,37 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { computed } from 'vue'
 
 const props = defineProps({
   type: { type:String, default:"" },
-  customTooltip: { type:String, default:"" }
+  customTooltip: { type:String, default:"" },
+  tooltips: { type:Object, default:{} }
 })
 
 const innerText = computed(() => {
-  if (props.type === "Normal"){
-    return ""
-  }
+  if (props.type === "Normal"){ return "" }
   return `(${props.type})`
 })
+
+const tooltips = {
+  "Normal":  "",
+  "Player":  "This entry represents a player character. Info shown here is highly subject to change. If you are the owner of this character and you see a mistake, please let your GM know.",
+  "Group":   "This entry represents a group of NPCs. Individuals may vary from info shown here.",
+  "Generic": "This entry represents multiple NPCs. Individuals may vary from info shown here.",
+  "Deity":   "This entry represents a deity. In-game lore may differ from standard lore.",
+  "Dragon":  "This entry represents a dragon.",
+}
 
 const tooltip = computed(() => {
   var result = ""
   if (props.customTooltip !== ""){
     result = props.customTooltip
   }
-  else if (props.type !== "Normal") {
-    if (props.type === "Player") { result = "This entry represents a player character. Info shown here is highly subject to change. If you are the owner of this character and you see a mistake, please let your GM know." }
-    else if (props.type === "Group") { result = "This entry represents a group of NPCs. Individuals may vary from info shown here." }
-    else if (props.type === "Generic") { result = "This entry represents multiple NPCs. Individuals may vary from info shown here." }
-    else if (props.type === "Deity") { result = "This entry represents a deity. In-game lore may differ from standard lore."}
-    else if (props.type === "Dragon") { result = "This entry represents a dragon."}
-    else { result = "This entry is unique." }
+  else {
+    try {
+      result = props.tooltips[props.type]
+    } catch (error) {
+      result = ""
+    }
   }
   return result
 })
@@ -35,7 +42,7 @@ const tooltip = computed(() => {
 </template>
 
 <style>
-#characterType {
+.characterType {
   text-align: center;
   margin-top: 0.1em;
 }
